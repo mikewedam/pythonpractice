7727a84262557fcbd50ce923af19847483ba707a
mikewedam <45424764+mikewedam@users.noreply.github.com>
2021-07-06 14:29:57 +0000

    Add files via upload

---
+++ Python code with Mosh/main.py
@@ -0,0 +1,135 @@
+# first_name = input("First Name? ")
+# print(first_name)
+
+# last_name = input("Last Name? ")
+# print(last_name)
+
+# age = input("Age? ")
+# print(age)
+
+# sex = input("Sex? ")
+# print(sex)
+
+# print("Patient Infomation")
+# print(first_name,last_name,age,sex)
+
+# CONVERTER
+# weight = input("Enter your weight? ")
+# k = 0.45 * int(weight)
+
+# print(k)
+
+# STRINGS
+# name = input("What is your name? ")
+# print("Hi " + name + " welcome to python is easy.")
+
+# message = '''
+# Mr.Michael Abocco,
+#   We were very impressed with your presentation.
+# We would like to invite you over to sign a contract.
+# Please send us a mail to arrange a meeting.
+# Best regards
+#  Management.
+# '''
+
+# print(message)
+
+# FORMATTED STRING
+# first_name = 'Michael'
+# last_name = 'Abocco'
+# msg = f'{first_name} [{last_name}] is a coder'
+# print(msg)
+
+
+# name = input("What is your name ")
+# text = name.find(name)
+# print (name)
+
+# num1 = input()
+# num2 = input()
+# print (int(num1) // int(num2))
+
+# x = (2 +3) * 10 - 3
+# print(x)
+
+# goodcredit = False
+# if goodcredit:
+#   print("You need to put down 10%")
+#  print(0.1 * 1000000)
+# else:
+# print("You need to put down 20%")
+# print(0.2 * 1000000)
+
+# price = 1000000
+# has_good_credit = False
+# if has_good_credit:
+# down_payment = 0.1 * price
+# print(f"down_payment ${down_payment}")
+# else:
+#   down_payment = 0.2 * price
+#  print(f"down_payment ${down_payment}")
+
+# poem = '''
+# Twinkle, twinkle, little star,
+# \n\tHow I wonder what you ar!
+# up above the world so high,
+# like a diamond in the sky.
+# Twinkle, twinkle, little star,
+# How I wonder what you are.
+# '''
+# print (poem)
+
+# WEIGHT CONVERTER PROGRAM
+
+
+# Calculator
+
+# num1 = input("Enter a number " )
+# unit = input("Enter a math operater " )
+# num2 = input("Enter a number " )
+# if unit == '+':
+#   print(int(num1) + int(num2))
+# elif unit == '-':
+#   print(int(num1) - int(num2))
+# elif unit == '*':
+#   print(int(num1) * int(num2))
+# elif unit == '//':
+#    print(int(num1) // int(num2))
+# elif unit == '%':
+#   print(int(num1) % int(num2))
+# else:
+#   print("Please enter a correct operator")
+
+cars = ["Mercedes", "BMW", "Volvo", "kia", "Audi", "Toyota", "Ford", "Nissan"]
+for car in cars:
+    print(type(cars))
+
+
+numbers = [10, 2, 3, 5, 4, 9, 8]
+
+max = numbers[0]
+for number in numbers:
+    if max < number:
+        max = number
+print(max)
+
+alist = [100, 200, 300, 400, 500]
+alist.reverse(alist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
