ae92f8669e49972793c6978a3c8a545f2e155201
mikewedam <45424764+mikewedam@users.noreply.github.com>
2021-07-09 10:40:35 +0000

    Add files via upload
    worked on loops for this update

--- main.py
+++ main.py
@@ -1,35 +1,122 @@
-print('''
-    Game Instructions
-    Start - To start car
-    Stop - To stop car
-    Exit - To end game
-''')
-car = "start"
-started = False
-while car == "start":
-    control = input(">")
-    if control == "start":
-        if started:
-            print("car already started")
-        else:
-            started = True
-            print("Car has started...")
-    elif control == "stop":
-        if control == "stop":
-            if not started:
-                print("car already stopped")
-            else:
-                started = False
-                print("Car has stopped")
-    elif control == "brake":
-        if control == "brake":
-            if not started:
-                print("brakes applied")
-            else:
-                started = False
-                print("Car has stopped")
-    elif control == "quit":
-        print("You quit the game.")
-        break
-    else:
-        print("I do not understand!")
+#CAR GAME
+#print('''
+    #Game Instructions
+    #Start - To start car
+    #Stop - To stop car
+    #Exit - To end game
+#''')
+#car = "start"
+#started = False
+#while car == "start":
+ #   control = input(">")
+  #        print("car already started")
+     #   else:
+      #      started = True
+       #     print("Car has started...")
+    #elif control == "stop":
+       # if control == "stop":
+        #    if not started:
+         #       print("car already stopped")
+          #  else:
+           #     started = False
+            #    print("Car has stopped")
+    #elif control == "brake":
+     #   if control == "brake":
+      #      if not started:
+       #         print("brakes applied")
+        #    else:
+         #       started = False
+          #      print("Car has stopped")
+    #elif control == "quit":
+     #   print("You quit the game.")
+      #  break
+    #else:
+     #   print("I do not understand!")
+
+#ROCK PAPER SCISSORS
+#print("Rock-Paper-Scissors Game")
+#print('''
+#> Rock beats scissors
+#> Scissors beats paper
+#> Paper beats rock
+#''')
+#player1 = input("> ")
+#player2 = input("> ")
+
+#while True:
+ #   if player1 == 'rock' and player2 == 'scissors':
+  #          print("Player1 wins")
+   # elif player1 == 'scissors' and player2 == 'rock':
+    #        print("Player2 wins")
+
+    #if player1 == 'scissors' and player2 == 'paper':
+     #       print("Player1 wins")
+    #elif player1 == 'paper' and player2 == 'scissors':
+     #       print("Player2 wins")
+
+    #if player1 == 'paper' and player2 == 'rock':
+     #       print("Player1 wins")
+    #elif player1 == 'rock' and player2 == 'paper':
+     #       print("Player2 wins")
+
+    #if player1 == 'paper' and player2 == 'paper':
+    #    player1 = input("> ")
+     #   player2 = input("> ")
+      #  continue
+    #if player1 == 'scissors' and player2 == 'scissors':
+     #   player1 = input("> ")
+      #  player2 = input("> ")
+       # continue
+    #if player1 == 'rock' and player2 == 'rock':
+     #   player1 = input("> ")
+      #  player2 = input("> ")
+       # continue
+    #break
+
+#else:
+    #print("Game Over")
+
+#DICTIONARIES
+#student = {
+ #   "name" : "Michael",
+  # "age" : 10,
+   # "class" : "Grade 6",
+#}
+#print(student)
+
+#LOOPS
+i = 0
+while i <= 10 :
+    print(i)
+    i += 1
+#LOOP THROUGH A LIST
+#thislist = ["Toyota","Mazda","Nissan"]
+#for car in thislist:
+ #   print(car)
+
+#thislist = ("Apple","Cherry","Banana")
+#for fruit in thislist:
+ #   print(fruit[0:6])
+
+#myset = ["Toyota","Mazda","Nissan"]
+#for car in myset:
+    #print(car)
+
+#phone = input("Phone Number : ")
+#number = {
+ #   "0" : "zero",
+  #  "1" : "one",
+   # "#2" : "two",
+    #"3" : "three",
+    #"4" : "four",
+    #"5" : "five",
+    #"6" : "six",
+    #"7" : "seven",
+    #"8" : "eight",
+    #"9" : "nine"
+#}
+#output = ""
+#for digit in phone:
+ #   output += number.get(digit, "!") + " "
+#print(output)
+
